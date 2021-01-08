6d16c82a3e595350960dfa44bef24df97f949502
DESKTOP-STRK401\hagirainbow <h8a9g11i@gmail.com>
2021-01-09 08:57:18 +0900

    .net側アプリ作成

---
+++ CSJar/CSJar/ConnectJar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSJar
+{
+    class ConnectJar
+    {
+        /// <summary>
+        /// .jar実行
+        /// </summary>
+        /// <param name="msg">メッセージ</param>
+        /// <returns>True:成功/False:失敗</returns>
+        public static bool Excecute(string msg)
+        {
+            bool result = false;
+            Process jar = null;
+            try
+            {
+                using (jar = Process.Start("javaw", "-jar Sample.jar"))
+                {
+                    // 終了待ち
+                    jar.WaitForExit();
+                    // 結果取得
+                    if (jar.ExitCode == 0) result = true;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("例外発生\n"+e.Message);
+            }
+            return result;
+        } 
+    }
+}
---
+++ CSJar/CSJar/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSJar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            ConnectJar.Excecute(textBox1.Text);
+            button1.Enabled = true;
+        }
+    }
+}
